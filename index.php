629095f68e72e43c1c7bc5ad8274150f42ca3310
meowngmeowng <michelle.espino@msumain.edu.ph>
2021-10-25 22:15:03 +0800

    initial

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>ICTC Tickets</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" >
+    <!-- Fonts -->
+    <link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome.min.css">
+    <!-- Icon -->
+    <link rel="stylesheet" type="text/css" href="assets/fonts/simple-line-icons.css">
+    <!-- Slicknav -->
+    <link rel="stylesheet" type="text/css" href="assets/css/slicknav.css">
+    <!-- Menu CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/menu_sideslide.css">
+    <!-- Slider CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/slide-style.css">
+    <!-- Nivo Lightbox -->
+    <link rel="stylesheet" type="text/css" href="assets/css/nivo-lightbox.css" >
+    <!-- Animate -->
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <!-- Main Style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <!-- Responsive Style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Unica+One&display=swap" rel="stylesheet">
+
+  </head>
+  <body>
+   
+      <!-- Hero Area Start -->
+      <div id="hero-area" class="hero-area-bg">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 text-center">
+              <div class="contents">
+                <h2 class="head-title wow fadeInUp" data-wow-delay="0.4s">Support Center </h2>
+                <h2 class="head-title wow fadeInUp" data-wow-delay="0.4s">Ticketing System</h2>
+                <p class="script-font wow fadeInUp" data-wow-delay="0.6s">Connect with us...</p>
+               
+                <div class="header-button wow fadeInUp" data-wow-delay="1s">
+                  <a href="https://tickets.msumain.edu.ph/support/" style="font-size:30px; margin: 20px; padding-top: 20px;padding-bottom:20px;padding-right: 110px;padding-left:110px; " class="btn btn-common">MSU Client</a>
+                  <a href="https://tickets.msumain.edu.ph/support/scp/" style="font-size:30px; margin: 20px; padding-top: 20px;padding-bottom:20px;padding-right: 110px;padding-left:110px; " class="btn btn-common">Tech Team</a>
+                </div>
+                 <div class="col-md-6 col-lg-6 col-sm-12">
+              
+            </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Hero Area End -->
+
+    </header>
+    <!-- Header Area wrapper End -->
+
+ 
+  </body>
+</html>
